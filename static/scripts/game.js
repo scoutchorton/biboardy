1d51b0bbf6e0c79771d7e27a8c8fff8812b04a8d
scoutchorton <scoutchorton@yahoo.com>
2021-03-06 02:34:26 -0500

    Removed debug code from game

--- static/scripts/game.js
+++ static/scripts/game.js
@@ -2,15 +2,6 @@ const { ipcRenderer } = require('electron');
 
 var app;
 var playerCap = 3;
-/* ### REMOVE ### */
-//var autoLoadData = true;
-//var autoLoadData = false;
-
-/* ### REMOVE ### */
-//var demoPlayers = ['1', '2', '3'];
-/* ### REMOVE ### */
-//var demoPlayers = [];
-//var demoPlayers = undefined;
 
 function fadeOut(element, length) {
 	//Only accept audio elements
@@ -68,18 +59,11 @@ window.addEventListener('load', () => {
 				if(mode === 'start' && app.$data.players.length > 1) {
 					app.$set(app.$data, 'playerupname', app.$data.players[Math.floor(Math.random() * app.$data.players.length)].name);
 					app.$set(app.$data, 'gamestate', 'jeopardy');
-					/* ### REMOVE ### */
-					//app.$set(app.$data, 'gamestate', 'double');
+
 					fadeOut(document.getElementById('audio--theme'), 1);
+
 					setTimeout(() => {
 						document.getElementById('audio--board').play();
-
-						/* ### REMOVE ### */
-						/*
-						setTimeout(() => {
-							//app.finishJeopardy();
-						}, 2000);
-						*/
 					}, 1000);
 				//Show score summary, sort of like an ad break
 				} else if(mode === "transition") {
@@ -243,27 +227,6 @@ window.addEventListener('load', () => {
 			},
 			'removeScore': (player) => {
 				player.score -= app.$data.currentQuestion.score;
-			},
-			/* ### REMOVE ### */
-			'finishJeopardy': () => {
-				console.group('clense');
-				let board = (app.$data.gamestate == "jeopardy") ? app.$data.data.boards.jeopardy : app.$data.data.boards.doublejeopardy;
-				for(let category of board.categories) {
-					console.log(`Clensing category ${category.name}...`);
-					console.groupCollapsed(category.name);
-					for(let questionIndex in category.questions) {
-						console.log(questionIndex);
-						app.$set(category.questions, questionIndex, undefined);
-					}
-					console.groupEnd();
-				}
-				console.groupEnd();
-				app.$set(board.categories[0].questions, 0, "Foo?");
-				app.openQuestion(board.categories[0], 0, 0);
-				setTimeout(() => {
-					//app.answerQuestion(app.$data.players[0], true);
-					app.closeQuestion();
-				}, 2000);
 			}
 		}
 	});
@@ -277,37 +240,6 @@ window.addEventListener('load', () => {
 		document.getElementById('audio--theme').volume = 0.25;
 		document.getElementById('audio--theme').play();
 	});
-	/* ### REMOVE ### */
-	/*
-	if(!autoLoadData) {
-		ipcRenderer.invoke('file', {
-			action: 'load'
-		}).then((data) => {
-			if(data !== undefined)
-				app.$set(app.$data, 'data', data);
-			document.getElementById('audio--theme').volume = 0.25;
-			if(autoPlayTheme)
-				document.getElementById('audio--theme').play();
-		});
-	} else {
-		let data = require('../../games/childrens_church_review.json');
-		app.$set(app.$data, 'data', data);
-	}
-	*/
-
-	/* ### REMOVE ### */
-	//Load demo players
-	/*
-	for(let player of demoPlayers) {
-		app.$refs.playerEntry.value = player;
-		app.addPlayer();
-	}
-	*/
-	/* ### REMOVE ### */
-	/*
-	if(demoPlayers)
-		app.gameProgress('start')
-	*/
 });
 
 //Listen for key presses
